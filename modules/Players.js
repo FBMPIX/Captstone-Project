c710145a2dc53b683bb95ebc4d3153020f8e59b4
FBMPIX <165311459+FBMPIX@users.noreply.github.com>
2026-02-19 14:00:32 -0500

    Logic: Modified 'getNextPlayer' so it can no longer provide negative Indices

--- modules/Players.js
+++ modules/Players.js
@@ -1,7 +1,7 @@
 class UnoGame {
   constructor(playerNames) {
     this.players = playerNames.map(name => ({ name, hand: [] }));
-    this.deck = []; 
+    this.deck = [];
     this.discardPile = [];
     this.currentPlayerIndex = 0;
     this.direction = 1; // 1 for clockwise, -1 for counter clockwise
@@ -12,12 +12,11 @@ class UnoGame {
   }
 
   getNextPlayer(steps = 1) {
-    // ((0+(1*1)+4)%4)-4  = 2
-    // moves clockwise 2 steps.
-    // ((0+(1*-1)+4)%4)-4 = -2
-    // moves counterclockwise 2 steps
+    return (this.currentPlayerIndex + (steps * this.direction) + this.players.length) % this.players.length;
+  }
 
-    return ((this.currentPlayerIndex + (steps * this.direction) + this.players.length) % this.players.length)-4;
+  reverseTurnOrder() {
+    this.direction *= -1;
   }
 }
 
